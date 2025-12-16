acfb28acfd182f2c85cd946d1039aec1fb53887c
Emily Xiong <xiongemi@gmail.com>
2025-12-16 15:29:04 -0500

    next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-zinc-50 dark:bg-black`}
       >
